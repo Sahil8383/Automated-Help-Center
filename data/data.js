0669d78233fea114f5157ecc597833dbd85c1a64
vedant-3010 <vedantdandgepro@gmail.com>
2024-03-27 14:55:42 +0530

    Added intent value input functionality

---
+++ data/data.js
@@ -0,0 +1,46 @@
+const data = {
+    "intents": [
+      {
+        "name": "ACCOUNT",
+        "message": "Manage account"
+      },
+      {
+        "name": "CANCELLATION_FEE",
+        "message": "Check cancellation fees"
+      },
+      {
+        "name": "DELIVERY",
+        "message": "Track delivery status"
+      },
+      {
+        "name": "FEEDBACK",
+        "message": "Share your feedback"
+      },
+      {
+        "name": "INVOICE",
+        "message": "View your invoice"
+      },
+      {
+        "name": "NEWSLETTER",
+        "message": "Subscribe for updates"
+      },
+      {
+        "name": "ORDER",
+        "message": "Confirm your order"
+      },
+      {
+        "name": "PAYMENT",
+        "message": "Complete payment"
+      },
+      {
+        "name": "REFUND",
+        "message": "Process refund"
+      },
+      {
+        "name": "SHIPPING_ADDRESS",
+        "message": "Update shipping address"
+      }
+    ]
+  }
+  
+    export default data;

a45540a250e387ebdc5d5eb4c2ec8e4e418018e7
Aryan Chavan <aryan@aryansdomain.com>
2024-03-27 19:47:41 +0530

    change class= to classname=

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -39,15 +39,15 @@ const Chat = () => {
 
 	return (
 		<>
-			<div class="flex h-screen overflow-hidden">
-				<div class="flex-1">
-					<header class="bg-white p-4 text-gray-700">
-						<h1 class="text-2xl font-semibold">
+			<div className="flex h-screen overflow-hidden">
+				<div className="flex-1">
+					<header className="bg-white p-4 text-gray-700">
+						<h1 className="text-2xl font-semibold">
 							Dunder Mifflin (Kelly)
 						</h1>
 					</header>
 
-					<div class="h-screen overflow-y-auto p-4 pb-36">
+					<div className="h-screen overflow-y-auto p-4 pb-36">
 						{/* <AiMessage message={"Hello! How may I help you?"} />
 
 						<UserMessage message={"I am a dumb user!"} /> */}
@@ -73,14 +73,14 @@ const Chat = () => {
 						{/* <IntentCard intent={intentData} /> */}
 					</div>
 
-					<footer class="bg-white border-t border-gray-300 p-4 absolute bottom-0 w-full">
-						<div class="flex items-center">
+					<footer className="bg-white border-t border-gray-300 p-4 absolute bottom-0 w-full">
+						<div className="flex items-center">
 							<input
 								type="text"
 								placeholder="Type a message..."
-								class="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500"
+								className="w-full p-2 rounded-md border border-gray-400 focus:outline-none focus:border-blue-500"
 							/>
-							<button class="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
+							<button className="bg-indigo-500 text-white px-4 py-2 rounded-md ml-2">
 								Send
 							</button>
 						</div>
@@ -93,16 +93,16 @@ const Chat = () => {
 
 const AiMessage = ({ message }) => (
 	<>
-		<div class="flex mb-4 cursor-pointer">
-			<div class="w-9 h-9 rounded-full flex items-center justify-center mr-2">
+		<div className="flex mb-4 cursor-pointer">
+			<div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
 				<img
 					src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
 					alt="User Avatar"
-					class="w-8 h-8 rounded-full"
+					className="w-8 h-8 rounded-full"
 				/>
 			</div>
-			<div class="flex max-w-96 bg-white rounded-lg p-3 gap-3">
-				<p class="text-gray-700">{message}</p>
+			<div className="flex max-w-96 bg-white rounded-lg p-3 gap-3">
+				<p className="text-gray-700">{message}</p>
 			</div>
 		</div>
 	</>
@@ -110,15 +110,15 @@ const AiMessage = ({ message }) => (
 
 const UserMessage = ({ message }) => (
 	<>
-		<div class="flex justify-end mb-4 cursor-pointer">
-			<div class="flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3">
+		<div className="flex justify-end mb-4 cursor-pointer">
+			<div className="flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3">
 				<p>{message}</p>
 			</div>
-			<div class="w-9 h-9 rounded-full flex items-center justify-center ml-2">
+			<div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
 				<img
 					src="https://placehold.co/200x/b7a8ff/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
 					alt="My Avatar"
-					class="w-8 h-8 rounded-full"
+					className="w-8 h-8 rounded-full"
 				/>
 			</div>
 		</div>
--- src/components/History.js
+++ src/components/History.js
@@ -1,39 +1,40 @@
 import React from "react";
 
 const History = () => {
-  return (
-    <>
-      <div class="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
-        <div class="w-12 h-12 bg-gray-300 rounded-full mr-3">
-          <img
-            src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
-            alt="User Avatar"
-            class="w-12 h-12 rounded-full"
-          />
-        </div>
-        <div class="flex-1">
-          <h2 class="text-lg font-semibold">Alice</h2>
-          <p class="text-gray-600">Hoorayy!!</p>
-        </div>
-      </div>
+	return (
+		<>
+			<div className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
+				<div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
+					<img
+						src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
+						alt="User Avatar"
+						className="w-12 h-12 rounded-full"
+					/>
+				</div>
+				<div className="flex-1">
+					<h2 className="text-lg font-semibold">Alice</h2>
+					<p className="text-gray-600">Hoorayy!!</p>
+				</div>
+			</div>
 
-      <div class="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
-        <div class="w-12 h-12 bg-gray-300 rounded-full mr-3">
-          <img
-            src="https://placehold.co/200x/ad922e/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
-            alt="User Avatar"
-            class="w-12 h-12 rounded-full"
-          />
-        </div>
-        <div class="flex-1">
-          <h2 class="text-lg font-semibold">Martin</h2>
-          <p class="text-gray-600">
-            That pizza place was amazing! We should go again sometime. 🍕
-          </p>
-        </div>
-      </div>
-    </>
-  );
+			<div className="flex items-center mb-4 cursor-pointer hover:bg-gray-100 p-2 rounded-md">
+				<div className="w-12 h-12 bg-gray-300 rounded-full mr-3">
+					<img
+						src="https://placehold.co/200x/ad922e/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
+						alt="User Avatar"
+						className="w-12 h-12 rounded-full"
+					/>
+				</div>
+				<div className="flex-1">
+					<h2 className="text-lg font-semibold">Martin</h2>
+					<p className="text-gray-600">
+						That pizza place was amazing! We should go again
+						sometime. 🍕
+					</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default History;

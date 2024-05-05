a8c2b041aa4b3509d0d668b796fd9a614446f4a7
Sahil8383 <sahilbhatt915@gmail.com>
2024-05-05 15:34:32 +0530

    added functions for intents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ChatBot from "./components/ChatBot";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Ecommerce from "./components/Ecommerce";
 
 function App() {
   return (
     <div className="App">
-      <ChatBot />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ChatBot />} />
+            <Route path="/ecommerce" element={<Ecommerce/>} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- src/components/IntentCard.js
+++ src/components/IntentCard.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const IntentCard = ({ intent }) => {
+  const baseUrl = "http://localhost:5000/orders";
+
   const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
@@ -8,8 +12,98 @@ const IntentCard = ({ intent }) => {
   };
 
   const handleSubmit = () => {
-    console.log("Input Value:", inputValue);
+    alert(`You have entered: ${inputValue}`);
   };
+
+  const [orders, setOrders] = useState([]);
+
+  // Cancel_order: change orderStatus to cancel
+  // change_order: change orderStatus to change ,point changed variable to new order number
+  // place_order: new order object
+  // payment_issue: display paymentStatus refundStatus orderStatus if payment issue flag payment status as issue
+  // track_refund:display refundstatus refundEta TotalAmount
+  // FEEDBACK: complaint, review {add complaint reivew}
+  // check_invoice:Dislay invoice number
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/orders");
+      setOrders(response.data);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
+  };
+
+  const cancelOrder = async (orderNumber) => {
+    try {
+      const order = await axios.get(`${baseUrl}/${orderNumber}`);
+      order.data.orderStatus = "cancelled";
+      await axios.put(`${baseUrl}/${orderNumber}`, order.data);
+      alert("Order cancelled successfully");
+    } catch (error) {
+      console.error("Error cancelling order:", error);
+    }
+  };
+  
+  const changeOrder = async (orderNumber) => {
+    try {
+      const order = await axios.get(`${baseUrl}/${orderNumber}`);
+      order.data.orderStatus = "changed";
+      await axios.put(`${baseUrl}/${orderNumber}`, order.data);
+      alert("Order changed successfully");
+    } catch (error) {
+      console.error("Error changing order:", error);
+    }
+  }
+
+  const placeOrder = async (order) => {
+    try {
+      await axios.post(baseUrl, order);
+      alert("Order placed successfully");
+    } catch (error) {
+      console.error("Error placing order:", error);
+    }
+  }
+
+  const paymentIssue = async (transactionId) => {
+    try {
+      const order = await axios.get(`${baseUrl}/${transactionId}`);
+      if(order.data.paymentStatus === "issue") {
+        alert(`Payment issue: ${order.data.paymentStatus}`);
+      }
+      else {
+        alert(`Payment status: ${order.data.paymentStatus}`);
+      }
+      alert(`Payment status: ${order.data.paymentStatus}`);
+    } catch (error) {
+      console.error("Error getting payment status:", error);
+    }
+  }
+
+
+  const trackRefund = async (orderNumber) => {
+    try {
+      const order = await axios.get(`${baseUrl}/${orderNumber}`);
+      alert(`Refund status: ${order.data.refundStatus}`);
+      alert(`Refund ETA: ${order.data.refundETA}`);
+      alert(`Total amount: ${order.data.totalAmount}`);
+    } catch (error) {
+      console.error("Error tracking refund:", error);
+    }
+  }
+
+  const checkInvoice = async (orderNumber) => {
+    try {
+      const order = await axios.get(`${baseUrl}/${orderNumber}`);
+      alert(`Invoice number: ${order.data.invoiceNumber}`);
+    } catch (error) {
+      console.error("Error checking invoice:", error);
+    }
+  }
 
   return (
     <div className="flex justify-end mb-4 cursor-pointer">
@@ -19,78 +113,108 @@ const IntentCard = ({ intent }) => {
         </div>
         <p>{intent.message}</p>
         <div>
-          {
-            intent === 'Order' && (
+          {intent === "cancel_order" && (
+            <>
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me order ID."
+                placeholder="Give me Order ID."
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          {
-            intent === 'payment_issue' && (
+              <button
+                onClick={() => cancelOrder(inputValue)}
+                className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Send
+              </button>
+            </>
+          )}
+          {intent === "change_order" && (
+            <>
+              <a
+                href="/ecommerce"
+                className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Buy Items
+              </a>
+            </>
+          )}
+           {intent === "place_order" && (
+            <>
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me payment ID."
+                placeholder="Give me Order Name"
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          {
-            intent === 'Shipment' && (
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me shipment ID."
+                placeholder="Give me Items"
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          {
-            intent === 'Refund' && (
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me refund ID."
+                placeholder="Give me Order Name"
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          {
-            intent === 'Return' && (
+              <button
+                onClick={() => cancelOrder(inputValue)}
+                className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Send
+              </button>
+            </>
+          )}
+          {(intent === "payment_issue" ||
+            intent === "check_payment_methods") && (
+            <>
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me return ID."
+                placeholder="Give me Transaction ID."
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          {
-            intent === 'Complaint' && (
+              <button
+                onClick={handleSubmit}
+                className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Send
+              </button>
+            </>
+          )}
+          {intent === "Shipment" && (
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Give me shipment ID."
+              className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
+            />
+          )}
+          {(intent === "check_refund_policy" || intent === "track_refund") && (
+            <>
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
-                placeholder="Give me complaint ID."
+                placeholder="Give me Order ID."
                 className="p-2 rounded-md border border-gray-400 focus:outline-none text-black focus:border-blue-500"
               />
-            )
-          }
-          <button
-            onClick={handleSubmit}
-            className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
-          >
-            Send
-          </button>
+              <button
+                onClick={handleSubmit}
+                className="bg-indigo-600 text-white px-4 py-2 rounded-md ml-2"
+              >
+                Send
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
